519494ebd4a18b749de818b078b8e4a499e40a73
ScanMst <ur226278@gmail.com>
2024-06-18 18:53:41 -0600

    inicio del proyecto

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace examen2Poo
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base (options)
+        {
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace examen2Poo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
